c96359346a5c65efc4c15c3c50b949d1929f6d0b
Oleg Gordienov <ogordienov@yandex.ru>
2022-08-01 21:14:38 +0200

    feat: add movies schema

---
+++ models/movie.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true
+  },
+  director: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  year: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+    }
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+    }
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+    }
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user'
+  },
+  movieId: {
+    type: String,
+    required: true
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isAlphanumeric(v, 'ru-RU', {ignore: ' -'}),
+    }
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isAlphanumeric(v, 'en-US', {ignore: ' -'}),
+    }
+  }
+});
+
+module.exports = mongoose.model('movie', movieSchema);
